974be3643c168505b77f1125f632f2de5c3f1025
T.Morio <center@morikapu.net>
2018-01-26 15:59:11 +0900

    Ver2.0

--- linetrace.c
+++ linetrace.c
@@ -8,31 +8,30 @@ main(void) {
 	TRISA = 0x1F;
 	TRISB = 0x00;
 	OPTION_REG = 0b00000111;
-	PULSE = 150;
+	PULSE = 150;		//サーボモーター初期位置定義
 	TMR0 = 61;
 	INTCON = 0b10100000;
 	PORTB = 0b00001000;
-	int pld = 150;
-	int flag = 0;
-	while (1) {
-		if (PORTAbits.RA4 == 1) {
-			flag = 1;
+	int pld = 150;		//サーボモーター位置変数定義
+	int flag = 0;		//起動フラグ定義
+	while (1) {			//ループ開始
+		if (PORTAbits.RA4 == 1) {	//起動スイッチ待機
+			flag = 1;				//起動フラグを設定
 		}
-		if (flag == 1) {
-
-			PORTBbits.RB7 = PORTBbits.RB5 = 0;
+		if (flag == 1) {			//起動フラグ確認
+			PORTBbits.RB7 = PORTBbits.RB5 = 0;	//前進
 			PORTBbits.RB6 = PORTBbits.RB4 = 1;
-			if (PORTAbits.RA0 == 0 && PORTAbits.RA2 == 1 && pld > 100) {
-				pld = pld - 10;
-				PULSE = pld;
+			if (PORTAbits.RA0 == 0 && PORTAbits.RA2 == 1 && pld > 100) {	//サーボ右回転用及び、回転可能上限の設定
+				pld = pld - 10;	//角度設定
+				PULSE = pld;	//サーボモーター動作実行
 			}
-			else if (PORTAbits.RA0 == 1 && PORTAbits.RA2 == 0 && pld < 200) {
-				pld = pld + 10;
-				PULSE = pld;
+			else if (PORTAbits.RA0 == 1 && PORTAbits.RA2 == 0 && pld < 200) {	//サーボ左回転用及び、回転可能上限の設定
+				pld = pld + 10;	//角度設定
+				PULSE = pld;	//サーボモーター動作実行
 			}
-			__delay_ms(1);
-			PORTBbits.RB7 = PORTBbits.RB5 = PORTBbits.RB6 = PORTBbits.RB4 = 0;
-			__delay_ms(2);
+			__delay_ms(4);		//4msウェイト
+			PORTBbits.RB7 = PORTBbits.RB5 = PORTBbits.RB6 = PORTBbits.RB4 = 0;	//モーター動作停止
+			__delay_ms(5);		//5msウェイト
 		}
 	}
 	return 0;
